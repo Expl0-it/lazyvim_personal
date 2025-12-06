e71ac19a4e21e3d07de64d382268ba7175100bb3
Expl0it <90573515+Expl0-it@users.noreply.github.com>
2025-12-06 15:40:06 +0100

    added config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,25 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- SCROLLOFF
+-- Minimal number of screen lines to keep above and below the cursor. (999 - always in the middle)
+vim.opt.scrolloff = 999
+
+-- WORDWRAP
+vim.opt.wrap = true
+vim.opt.textwidth = 80
+-- Word wrap at word end, not at the last character fitting the line
+vim.opt.linebreak = true
+
+-- TAB BEHAVIOUR
+-- Set tab to behave as tab not spaces
+vim.opt.expandtab = false
+-- Put non expanded tab on Shift + Tab
+vim.keymap.set("i", "<S-Tab>", "<C-V><Tab>")
+-- Set tab length to x spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- 	AUTOFORMAT
+vim.g.autoformat = true
---
+++ lua/plugins/import.lua
@@ -0,0 +1,7 @@
+return {
+  { import = "plugins.themes" },
+--  { import = "plugins.lang" },
+  { import = "plugins.builtin" },
+--  { import = "plugins.42" },
+  --  { import = "plugins.own" },
+}
